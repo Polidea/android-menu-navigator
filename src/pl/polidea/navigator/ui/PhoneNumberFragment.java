2ba84448fa556895a6a44382e5bc5d694211d9bc
Jarek Potiuk <jarek.potiuk@polidea.pl>
2011-05-15 16:01:14 +0200

    Fast clicks disallowed on contact intent launching.

--- src/pl/polidea/navigator/ui/PhoneNumberFragment.java
+++ src/pl/polidea/navigator/ui/PhoneNumberFragment.java
@@ -18,7 +18,6 @@ import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 
@@ -31,25 +30,33 @@ public class PhoneNumberFragment extends AbstractNumberFragment {
     protected static final int CONTACT_PICKER_RESULT = 1234;
     private EditText editText;
 
+    private View contactImage;
+
     @Override
     public PhoneNumberMenu getNavigationMenu() {
         return (PhoneNumberMenu) super.getNavigationMenu();
     }
 
     @Override
-    public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater,
+            final ViewGroup container, final Bundle savedInstanceState) {
         if (getNavigationMenu() == null) {
             return null;
         }
-        final ViewGroup layout = (ViewGroup) inflater.inflate(R.layout.phone_number_layout_fragment, container, false);
-        editText = (EditText) layout.findViewById(R.id.provide_phone_number_text);
-        final ImageButton contactImage = (ImageButton) layout.findViewById(R.id.select_from_contacts);
-        final Button nextButton = (Button) layout.findViewById(R.id.provide_phone_number_button);
+        final ViewGroup layout = (ViewGroup) inflater.inflate(
+                R.layout.phone_number_layout_fragment, container, false);
+        editText = (EditText) layout
+                .findViewById(R.id.provide_phone_number_text);
+        contactImage = layout.findViewById(R.id.select_from_contacts);
+        final Button nextButton = (Button) layout
+                .findViewById(R.id.provide_phone_number_button);
         final PhoneNumberMenu menu = getNavigationMenu();
-        editText.setImeOptions(EditorInfo.TYPE_CLASS_PHONE | EditorInfo.IME_ACTION_NEXT);
+        editText.setImeOptions(EditorInfo.TYPE_CLASS_PHONE
+                | EditorInfo.IME_ACTION_NEXT);
         editText.setOnEditorActionListener(new OnEditorActionListener() {
             @Override
-            public boolean onEditorAction(final TextView v, final int actionId, final KeyEvent event) {
+            public boolean onEditorAction(final TextView v, final int actionId,
+                    final KeyEvent event) {
                 return goNext(v.getText().toString());
             }
 
@@ -63,8 +70,11 @@ public class PhoneNumberFragment extends AbstractNumberFragment {
         contactImage.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(final View v) {
-                final Intent contactPickerIntent = new Intent(Intent.ACTION_PICK, Phone.CONTENT_URI);
-                startActivityForResult(contactPickerIntent, CONTACT_PICKER_RESULT);
+                v.setEnabled(false);
+                final Intent contactPickerIntent = new Intent(
+                        Intent.ACTION_PICK, Phone.CONTENT_URI);
+                startActivityForResult(contactPickerIntent,
+                        CONTACT_PICKER_RESULT);
             }
         });
         nextButton.setOnClickListener(new OnClickListener() {
@@ -78,7 +88,8 @@ public class PhoneNumberFragment extends AbstractNumberFragment {
     }
 
     @Override
-    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+    public void onActivityResult(final int requestCode, final int resultCode,
+            final Intent data) {
         if (resultCode == Activity.RESULT_OK) {
             switch (requestCode) {
             case CONTACT_PICKER_RESULT:
@@ -88,14 +99,15 @@ public class PhoneNumberFragment extends AbstractNumberFragment {
                 // do nothing
             }
         } else {
-            // gracefully handle failure
             Log.w(TAG, "Warning: activity result not ok");
         }
+        contactImage.setEnabled(true);
     }
 
     private void handleContactResult(final Intent data) {
         final String[] proj = { Phone.NUMBER, Phone.TYPE };
-        final Cursor cursor = getActivity().managedQuery(data.getData(), proj, null, null, null);
+        final Cursor cursor = getActivity().managedQuery(data.getData(), proj,
+                null, null, null);
         if (cursor.moveToFirst()) {
             editText.setText(cursor.getString(0));
         }
