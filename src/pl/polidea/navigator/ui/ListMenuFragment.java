acc1bfb9600c60016161309655224670af938bd6
Marek Multarzynski <Marek.Multarzynski@polidea.pl>
2012-10-23 19:33:46 +0200

    working on last selected items in list view

--- src/pl/polidea/navigator/ui/ListMenuFragment.java
+++ src/pl/polidea/navigator/ui/ListMenuFragment.java
@@ -1,11 +1,15 @@
 package pl.polidea.navigator.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import pl.polidea.navigator.R;
 import pl.polidea.navigator.menu.AbstractNavigationMenu;
 import pl.polidea.navigator.menu.ListMenu;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -77,6 +81,58 @@ public class ListMenuFragment extends AbstractMenuNavigatorFragment {
         }
     }
 
+    private class LatestListAdapter extends BaseAdapter {
+
+        private final TextView latestText;
+        // first = description, second = transaction
+        private List<Pair<String, String>> dTList;
+        private final LayoutInflater inflater;
+
+        /**
+         * @param descriptionTransactionPairsList
+         *            first = description, second = transaction
+         */
+        public LatestListAdapter(final List<Pair<String, String>> descriptionTransactionPairsList,
+                final TextView latestText, final LayoutInflater inflater) {
+            this.dTList = descriptionTransactionPairsList;
+            this.inflater = inflater;
+            if (dTList == null) {
+                dTList = new ArrayList<Pair<String, String>>();
+            }
+
+            this.latestText = latestText;
+            if (dTList.isEmpty()) {
+                latestText.setVisibility(View.GONE);
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return dTList.size();
+        }
+
+        @Override
+        public Object getItem(final int position) {
+            return dTList.get(position);
+        }
+
+        @Override
+        public long getItemId(final int position) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        @Override
+        public View getView(final int position, final View convertView, final ViewGroup parent) {
+            final View listItemView = inflater.inflate(R.layout.single_list_item_layout, null);
+            final ImageView imageView = (ImageView) listItemView.findViewById(R.id.list_item_image);
+            final TextView textView = (TextView) listItemView.findViewById(R.id.list_item_text);
+            imageView.setVisibility(View.GONE);
+            textView.setText(dTList.get(position).first);
+            return listItemView;
+        }
+    }
+
     @Override
     public ListMenu getNavigationMenu() {
         return (ListMenu) super.getNavigationMenu();
@@ -91,8 +147,8 @@ public class ListMenuFragment extends AbstractMenuNavigatorFragment {
         final ListView listView = (ListView) listViewGroup.findViewById(R.id.listView);
         final TextView latestText = (TextView) listViewGroup.findViewById(R.id.latestChoosenTextView);
         final ListView latestChoosenListView = (ListView) listViewGroup.findViewById(R.id.latestChoosenListView);
-        // latestText.setVisibility(View.GONE);
         listView.setAdapter(new ListMenuAdapter(getNavigationMenu(), inflater));
+        latestChoosenListView.setAdapter(new LatestListAdapter(null, latestText, inflater));
 
         return listViewGroup;
     }
